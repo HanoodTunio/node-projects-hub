7654a80f6e977a8995a02ad30e1cef2bbe4cdc42
hanood_tunio <hanood.tunio@gmail.com>
2025-02-16 14:23:41 +0500

    A short demo of the ejs template

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const connectDB = require('./connect');
 const urlRoutes = require('./routes/url'); // URL route imports
+const path = require("path")
+const URL = require("./model/url")
+
 const app = express();
 const PORT = 8001;
 
 // Connect to MongoDB
 connectDB('mongodb://127.0.0.1:27017/short-url');
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("views"));
+
 // Middleware setup
 app.use(express.json()); // For parsing application/json
+
+
+app.use("/test", async (req, res) => {
+    const allUrls = await URL.find({})
+    return res.render('home', { urls: allUrls });
+});
 
 // URL routes
 app.use('/url', urlRoutes);
