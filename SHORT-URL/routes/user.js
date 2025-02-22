11f0c51403027c534e91d93e3ff4fce298984fa6
hanood_tunio <hanood.tunio@gmail.com>
2025-02-22 18:07:48 +0500

    singup route

--- SHORT-URL/routes/user.js
+++ SHORT-URL/routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express")
+const handleUserSignUp = require("../controllers/user");
 
 const router = express.Router()
 
-router.post('/')
+router.post('/', handleUserSignUp);
 
 module.exports = router
