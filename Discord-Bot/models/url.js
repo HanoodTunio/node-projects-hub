6a2d99ec2b98e53819529fe252dce99f7c45d081
hanood_tunio <hanood.tunio@gmail.com>
2025-02-26 23:26:12 +0500

    create schema of the url

---
+++ Discord-Bot/models/url.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// URL schema
+const urlSchema = new mongoose.Schema({
+    shortID: {
+        type: String,
+        required: true,
+        unique: true, // Ensures that short ID is unique
+    },
+    redirectURL: {
+        type: String,
+        required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+}, { timestamps: true }); // Timestamps will store createdAt and updatedAt
+
+// Create model based on the schema
+const URL = mongoose.model("URL", urlSchema);
+
+module.exports = URL;
