397b869b8e1a0188b8c32743433031b715962596
hanood_tunio <hanood.tunio@gmail.com>
2025-02-26 22:01:53 +0500

    created a new command ("ping")

--- Discord-Bot/index.js
+++ Discord-Bot/index.js
@@ -1,5 +1,8 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+console.log("TOKEN:", process.env.DISCORD_BOT_TOKEN); // Debugging output
+console.log("CLIENT ID:", process.env.DISCORD_BOT_CLIENT_ID);
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
@@ -8,8 +11,14 @@ const client = new Client({
 client.on("messageCreate", (message) => {
     if (message.author.bot) return;
     message.reply({
-        content: "Hi From Bot,"
+        content: "Hi From Bot!",
     });
 });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+
 
 client.login(process.env.DISCORD_BOT_TOKEN);
