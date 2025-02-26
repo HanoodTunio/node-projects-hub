bae9569920bf84c0dd1f39b50c81ecb7ff54c035
hanood_tunio <hanood.tunio@gmail.com>
2025-02-27 01:05:07 +0500

    added path of the views

--- BloggingApplicationWithMongoDB/index.js
+++ BloggingApplicationWithMongoDB/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
 
-
-
+app.set("view engines", "ejs");
+app.set('views', path.resolve("./views"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
