35014628b97df7a08c91ac7592e674b4c165597f
hanood_tunio <hanood.tunio@gmail.com>
2025-03-02 03:20:02 +0500

    MongoDb connected

--- BloggingApplicationWithMongoDB/index.js
+++ BloggingApplicationWithMongoDB/index.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user");
+const mongoose = require("mongoose")
+
+
 const app = express();
 
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogify")
+    .then((e) => { console.log("mongo connected") })
+    .catch((e) => { console.log("mongo not connected") })
 
 app.get('/', (req, res) => {
     res.render("home");
