9987bafaef00c5728c9d472ba17c65e52750ff8a
hanood_tunio <hanood.tunio@gmail.com>
2025-03-02 13:07:30 +0500

    added signin route

--- BloggingApplicationWithMongoDB/routes/user.js
+++ BloggingApplicationWithMongoDB/routes/user.js
@@ -11,6 +11,23 @@ router.get("/signup", (req, res) => {
     return res.render("signup")
 })
 
+router.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Await the promise to resolve the user data
+        const user = await User.matchPassword(email, password);
+
+        console.log("User:", user);
+
+        return res.redirect("/");
+    } catch (error) {
+        console.error(error.message);
+        return res.status(401).send("Invalid email or password");
+    }
+});
+
+
 router.post("/signup", async (req, res) => {
     const { fullName, email, password } = req.body;
     await User.create({ fullName, email, password });
